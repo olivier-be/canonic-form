ed158e6916a7b5762a1db2d31d58a0f5a1395162
olivier-be <61296476+olivier-be@users.noreply.github.com>
2020-10-27 13:37:39 +0100

    Add files via upload

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -8,7 +8,7 @@ using namespace std;
 float ante(float a)
 {
 	float y, x1, x2;
-	for (y = 100; y = 100; y++);
+	for (y = 100;; y++);
 	{
 		float a, x1;
 		if (a * y * y == 0)
@@ -18,7 +18,7 @@ float ante(float a)
 		}
 
 	}
-	for (y = 100; y = -100; y++);
+	for (y = 100;; y--);
 	{
 		float a, x2;
 		if (a * y * y == 0)
@@ -31,7 +31,7 @@ float ante(float a)
 
 }
 
-float alpha (float x1,float x2)
+float alpha(float x1, float x2)
 {
 	float alpha1;
 	if (x1 > 0 and x2 > 0 or x1 + x2 > 0)
@@ -41,7 +41,7 @@ float alpha (float x1,float x2)
 	else
 		return (x1 - x2);
 }
-float beta( float x1, float x2)
+float beta(float x1, float x2)
 {
 	float beta1;
 	while (x1 == x2)
@@ -55,7 +55,9 @@ float beta( float x1, float x2)
 
 int main()
 {
-	float a, b, c, i, y, x1, x2, z, alpha1, beta1;
+	float a, b, c, i, y, x1,x22,x11, x2, z, alpha1, beta1;
+	x11 = 0;
+	x22 = 0;
 	cout << "vous deverer ecrire de maniere lineaire par ex x au carre =x*x sinon x*1 et pareil pour ax =1* x\n";
 	cout << "la forme est:ax au carre - bx+c=1\n" << "ou ax au carre + bx+c=2\n" << "ou ax au carre - bx -c=3\n" << "ou ax au carre - bx-c=1 ax-bx+c=2\n";
 	cin >> i;
@@ -66,11 +68,11 @@ int main()
 	cout << "juste c \n";
 	cin >> c;
 	ante(a);
-	x1 = ante(x1);
-	x2 = ante(x2);
-	alpha(x1, x2);
-	beta(x1, x2);
-	cout << a << "( x - " << alpha1 << " )Â² +" << beta1 << endl;
+	x1 = ante(x11);
+	x2 = ante(x22);
+	alpha1=alpha(x1, x2);
+	beta1=beta(x1, x2);
+	cout << a << "( x - " << alpha1 << " )² +" << beta1 << endl;
 
 	return 0;
 }
